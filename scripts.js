1e181c9d38cdda1eb8b29e0b8e504bc23c9fb525
Oscar <orss3214@gmail.com>
2025-06-11 21:40:21 -0400

    navigation done

---
+++ scripts.js
@@ -0,0 +1,30 @@
+const hamburger = document.querySelector('.hamburger');
+const sidebar = document.querySelector('header nav');
+const links = document.querySelectorAll('header a');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    sidebar.classList.toggle('active');
+});
+
+window.addEventListener('resize', () => {
+    if (window.matchMedia('(max-width: 400px)').matches)
+    {
+        closeSidebar();
+    }
+});
+
+if(window.matchMedia('(max-width: 400px)').matches)
+{
+    closeSidebar();
+}
+
+function closeSidebar() 
+{
+    links.forEach(link => {
+        link.addEventListener('click', () => {
+            hamburger.classList.remove('active');
+            sidebar.classList.remove('active');
+        });
+    });
+}
